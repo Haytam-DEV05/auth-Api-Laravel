31f139f22e3760940521d201a8c9ed7e845e2b2d
Haytam-DEV05 <nefalhaytam57@gmail.com>
2026-06-12 00:05:57 +0100

    update the system auth, and make some update in the cors.php file

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function register(UserRequest $request)
     {
-        // $validated = $request->validated();
         User::create($request->validated());
 
         return response()->json([
@@ -27,30 +26,34 @@ class UserController extends Controller
             'password' => 'required|min:8'
         ]);
 
-        $user = User::where('email', $request->input('email'))->first();
-
-        if (!$user) {
+        if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'status' => false,
-                'message' => 'Invalid Email or Password !'
+                'message' => 'Email or Password Incorrect !'
             ]);
         }
+        $user = Auth::user();
 
-        if (!Hash::check($request->input('password'), $user->password)) {
-            return response()->json([
-                'status' => false,
-                'message' => 'Invalid Email or Password !'
-            ]);
-        }
-
-        $token = $user->createToken('auth_token')->plainTextToken;
+        $request->session()->regenerate();
 
         return response()->json([
             'status' => true,
             'message' => 'User Login Successfuly !',
             'data' => $user,
-            'token' => $token
         ]);
     }
 
+    public function logout(Request $request)
+    {
+        // $request->user()->currentAccessToken()->delete();
+        Auth::guard('web')->logout();
+        
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Logout Successfuly !',
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,5 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout'])
+    ->middleware('auth:sanctum');
