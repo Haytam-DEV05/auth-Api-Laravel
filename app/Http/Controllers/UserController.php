f6ca4c3e58d4c9b189ab30203775076c24b236a7
Haytam-DEV05 <nefalhaytam57@gmail.com>
2026-06-12 21:15:20 +0100

    make the function addImage in the UserController to allow the user add an image

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -47,13 +48,31 @@ class UserController extends Controller
     {
         // $request->user()->currentAccessToken()->delete();
         Auth::guard('web')->logout();
-        
+
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
         return response()->json([
             'status' => true,
             'message' => 'Logout Successfuly !',
+        ]);
+    }
+    public function addImage(Request $request)
+    {
+        $request->validate(['image' => 'required|image']);
+        $user = Auth::user();
+
+        if ($user->image) {
+            Storage::disk('imageProfile')->delete($user->image);
+        }
+        $image_path = $request->file('image')->store('imageProfile', 'public');
+
+        $user->update([
+            'image' => $image_path
+        ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'Image Saved Successfuly !',
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,16 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return response()->json([
-        'user' => $request->user()
-    ]);
-})->middleware('auth:sanctum');
 
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
-Route::post('/logout', [UserController::class, 'logout'])
-    ->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return response()->json([
+            'user' => $request->user()
+        ]);
+    });
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/addImage', [UserController::class, 'addImage']);
+});
